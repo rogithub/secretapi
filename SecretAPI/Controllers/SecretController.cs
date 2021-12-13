23a7ce69e7d6e16bfc884e429aa6cba025fe520e
rockdrigo <correo.rodrigo@gmail.com>
2021-12-13 14:28:25 -0600

    methods

--- SecretAPI/Controllers/SecretController.cs
+++ SecretAPI/Controllers/SecretController.cs
@@ -18,9 +18,55 @@ public class SecretController : ControllerBase
 	_secretsRepo = secretsRepo;
     }
 
-    [HttpGet("{user:guid}")]
-    public Task<IEnumerable<Secret>> GetAll()
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Create(Secret secret)
     {
-        return _secretsRepo.GetAll(Guid.NewGuid());
+        var value = await _secretsRepo.Create(secret);
+	if (value != 1)
+	{
+	    return BadRequest();
+	}
+
+	return CreatedAtAction(nameof(GetOne), new { user = secret.UserId, id = secret.Id }, secret);
     }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> Update(Secret secret)
+    {
+	var it = await _secretsRepo.GetOne(secret.UserId, secret.Id);
+	if (it == null)
+	{
+	    return NotFound();
+	}
+	
+        var val = await _secretsRepo.Update(secret);
+	return Ok(val);
+    }
+
+
+    [HttpGet("{user:guid}")]
+    public Task<IEnumerable<Secret>> GetAll(Guid user)
+    {
+        return _secretsRepo.GetAll(user);
+    }
+
+    [HttpGet("{user:guid}/{secret:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetOne(Guid user, Guid secret)
+    {        
+        var it = await _secretsRepo.GetOne(user, secret);
+	if (it == null)
+	{
+	    return NotFound();
+	}
+
+	return Ok(it);
+    }
+
 }
